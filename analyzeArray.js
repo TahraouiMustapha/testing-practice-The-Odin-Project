8eb013b038a712d4dbf189a2cce32d0d01af9394
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-01-28 13:30:54 +0100

    test : add tests to analyzeArray module

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,3 +1,3 @@
 export const analyzeArray = function(array) {
-    
+
 }
--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -1,3 +1,22 @@
 import { analyzeArray } from "./analyzeArray";
 
-it('works', () => {})
+describe('analyzeArray',() => {
+    test('it doesn\'t accept non-array argument', () => {
+        expect(() => analyzeArray(null)).toThrow("You must input an array of numbers!");
+        expect(() => analyzeArray(2)).toThrow("You must input an array of numbers!");
+    })
+
+    test.skip('it doesn\'t accept an array within a non-number value', () => {
+        expect(()=> analyzeArray([1, 'hi', 3, '3'])).toThrow('You must input an array of numbers!');
+    })
+
+    test.skip('it works with array of numbers', ()=> {
+        expect(analyzeArray([1,8,3,4,2,6])).toEqual({
+            average: 4,
+            min: 1,
+            max: 8,
+            length: 6
+         });
+
+    })
+})
